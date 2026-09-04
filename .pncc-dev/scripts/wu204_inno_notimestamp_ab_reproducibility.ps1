8cedf87aba5b5efc59035055df6496f234b76cf1
kmephis-ai <kmephis@gmail.com>
2026-09-04 23:51:13 +0300

    PIPE-WU-204: satisfy PowerShell UTF-8 BOM safety gate

--- .pncc-dev/scripts/wu204_inno_notimestamp_ab_reproducibility.ps1
+++ .pncc-dev/scripts/wu204_inno_notimestamp_ab_reproducibility.ps1
@@ -1,4 +1,4 @@
-Set-StrictMode -Version Latest
+﻿Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 $CompilerUrl = 'https://github.com/jrsoftware/issrc/releases/download/is-7_1_0/innosetup-7.1.0-x64.exe'
